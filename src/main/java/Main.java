5b45ea93b52833bb257fc1bd619fcd1ef1dbd718
Andrii Kutil <Andrey_Kutil@service-team.biz>
2021-12-29 20:49:46 +0200

    log4j2

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class Main {
+  private static final Logger logger = LogManager.getLogger(Main.class);
+  public static void main(String[] args) {
+    logger.info("TEST log4j2");
+  }
+}
